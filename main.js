226b43ba34c6c34e136ba19b18868be68d50c926
Dilshod2010 <dilshodjon.fahriddinov@gmail.com>
2024-12-24 19:08:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const ism = prompt("string")
+const num = +prompt("raqam")
+const hayoq = prompt("boolean")
+const underfund = prompt("undefined")
+const mega =prompt("null")
+const symbol = prompt("symbol")
+const begint = prompt("begint")
+const object = prompt("object")
+
+console.log(`${ism} ${typeof ism } malumot turiga kiradi
+    ${num} ${typeof num } malumot turiga kiradi
+    ${hayoq} ${typeof Boolean(hayoq) } malumot turiga kiradi
+    ${underfund} ${typeof undefined} malumot turiga kiradi
+    ${mega} ${typeof null} malumot turiga kiradi
+    ${symbol} ${typeof symbol} malumot turiga kiradi}
+    ${begint} ${typeof begint} malumot turiga kiradi
+    ${object} ${typeof object} malumot turiga kiradi`)
